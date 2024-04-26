fdc7661684de3d13c429b4e7653fae2278eaf23a
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2024-04-26 23:15:33 +0600

    update

--- app/streamtape.py
+++ app/streamtape.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Any
+from typing import Optional,Dict,List,Any
 import  requests
 
 # Streamtape class
@@ -13,6 +13,7 @@ class Streamtape:
 
     self.api_key = api_key
     self.api_secret = api_secret
+    self.login=f"login={self.api_key}&key={self.api_secret}"
     
   
   def _request(self,method:str,endpoint:str,params:Dict[str,Any]={})->Dict[str,Any]:
@@ -24,9 +25,41 @@ class Streamtape:
     
 
   def account_info(self)->Dict[str,Any]:
-    url=f"{self.baseurl}account/info?login={self.api_key}&key={self.api_secret}"
+    url=f"{self.baseurl}account/info?{self.login}"
     return self._request("GET",url)
   
   def download_ticket(self,fileid:str)->Dict[str,Any]:
-    url=f"{self.baseurl}file/dlticket?file={fileid}"
-
+    url=f"{self.baseurl}dlticket?file={fileid}&{self.login}"
+    return self._request("GET",url)
+  
+  def download_link(self,fileid:str,ticketid:str)->Dict[str,Any]:
+    url=f"{self.baseurl}file/dl?file={fileid}&ticket={ticketid}"
+    return self._request("GET",url)
+  
+  def file_info(self,file_id:str)->Dict[str,Any]:
+    url=f"{self.baseurl}file/info?file={file_id}&{self.login}"
+    return self._request("GET",url)
+  
+  def upload_file(self,
+                  file:str,
+                  folder_id:Optional[str]=None,
+                  hash:Optional[str]=None,
+                  httponly:Optional[bool]=None)->Dict[str,Any]:
+    url=f"{self.baseurl}/file/ul?{self.login}&&folder={folder_id}&sha256={hash}&httponly={httponly}"
+    
+    return self._request("POST",url,params=file)
+  
+  def add_remote_upload(self,
+    link:str,
+    folder_id:Optional[str]=None,
+    header:Optional[str]=None,
+    name:Optional[str]=None)->Dict[str,Any]:
+    url=f"{self.baseurl}remotedl/add?{self.login}&url={link}&folder={folder_id}&headers={header}amp;name={name}"
+    
+    return self._request("POST",url)
+  
+  def remove_upload_upload(self,
+                           id:str)->Dict[str,Any]:
+    url=f"{self.baseurl}remotedl/remove?{self.login}&id={id}"
+    
+    
