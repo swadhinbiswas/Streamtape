9ff0a8530bcf96a616e0f5ce316fcfacbc24e4dd
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2024-04-27 15:35:12 +0600

    docs update

---
+++ docs/html/_sources/pystreamtape.rst
@@ -0,0 +1,21 @@
+pystreamtape package
+====================
+
+Submodules
+----------
+
+pystreamtape.streamtape module
+------------------------------
+
+.. automodule:: pystreamtape.streamtape
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: pystreamtape
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
+
+
 setup(
+   
     name="Streamtape",
     version="1.0.0",
     description="Unofficial Streamtape API Wrapper for Python by Swadhin Biswas",
@@ -28,3 +31,18 @@ setup(
     ],
     zip_safe=False,
 )
+
+
+"""This is the setup.py file for the Streamtape API Wrapper. It contains all the metadata about the package.
+    The metadata includes the name, version, description, long description, keywords, URL, author, author email, packages, install_requires, classifiers, and zip_safe.
+    The metadata is used by PyPI to display information about the package.
+    
+    The setup() function is used to create a package distribution. It takes the following arguments:
+    - name: The name of the package.
+    - version: The version of the package.
+    - description: A short description of the package.
+    - long_description: A long description of the package.
+    - long_description_content_type: The content type of the long description.
+    - keywords: Keywords related to the package.
+    - url: The URL of the package.
+    """
